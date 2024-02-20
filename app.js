46c7b83cf67f14ed26c757a777391712b7257e62
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-20 13:00:03 +0100

    function next() for middleware

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express=require('express');
 
 const app= express();
+app.use((req, res, next)=>{
+    console.log("request received");
+    next();
+});
 
+app.use((req, res, next)=>{
+    res.status(201);
+    next();
+});
+app.use((request, response,next)=>{
+    response.json({message:"Your request app is sent  successfully!"});
+});
 module.exports=app;
