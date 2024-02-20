18fb7063c90be6c2a3ce3518cead114e66edfb15
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-20 16:28:33 +0100

    added CORS and api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 
 const app= express();
-app.use((req, res, next)=>{
+/*app.use((req, res, next)=>{
     console.log("request received");
     next();
 });
@@ -13,5 +13,32 @@ app.use((req, res, next)=>{
 app.use((request, response,next)=>{
     response.json({message:"Your request app is sent  successfully!"});
     console.log("Your request app is sent  successfully!")
+});*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
 module.exports=app;
